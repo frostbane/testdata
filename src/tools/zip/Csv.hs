98d16f7b3a067ff91a58b6e45ba3e403f7970e17
ǝuɐqʇsoɹɟ <frostbane@programmer.net>
2022-02-06 14:59:53 +1000

    open csv file
    --HG--
    branch : develop

--- src/tools/zip/Csv.hs
+++ src/tools/zip/Csv.hs
@@ -10,6 +10,8 @@
 
 module Csv
     ( checkFileExist,
+      openCsvFile,
+      closeCsvFile,
       parse,
     )
   where
@@ -21,10 +23,10 @@ import System.Directory
       doesFileExist,
     )
 import System.IO
-    ( hSetEncoding,
-      stdin,
-      stdout,
-      utf8,
+    ( openFile,
+      hClose,
+      Handle,
+      IOMode (ReadMode, WriteMode),
     )
 import Data.Typeable (typeOf)
 import qualified Data.Text as T
@@ -36,16 +38,61 @@ import Data.Text.Encoding
 import Data.Function
 import Control.Monad (liftM)
 import Control.Monad.IO.Class (liftIO)
+import Control.Exception
+    ( try,
+      SomeException,
+      displayException,
+    )
 
+{- | Check if the file exists
+    -}
 checkFileExist :: ()
-               => T.Text
+               => Either T.Text T.Text      -- ^ either the previous error or a filename
                -> IO (Either T.Text T.Text)
-checkFileExist fileName = do
-    exist <- doesFileExist $ T.unpack fileName
-    if exist
-       then return $ Right fileName
-       else return $ Left $ "'" <> fileName <> "' file does not exist."
+checkFileExist eiFileName = do
+    case eiFileName of
+        Left err       -> return $ Left err
+        Right fileName -> do
+            exist <- doesFileExist $ T.unpack fileName
+            if not exist
+               then return $ Left $ "'" <> fileName <> "' file does not exist."
+               else return $ Right fileName
 
-parse :: IO ()
-parse = do
-    return ()
+openCsvFile :: ()
+         => Either T.Text T.Text      -- ^ either previous error or filename
+         -> IOMode
+         -> IO (Either T.Text Handle)
+openCsvFile eiFileName mode = do
+    case eiFileName of
+        Left err       -> return $ Left err
+        Right fileName -> do
+            result <- try (openFile filePath mode) :: IO (Either SomeException Handle)
+            return $
+                case result of
+                    Left ex      -> Left $ T.pack $ displayException ex
+                    Right handle -> Right handle
+          where
+                filePath = T.unpack fileName
+
+closeCsvFile :: ()
+             => Either T.Text Handle
+             -> IO (Either T.Text (IO ()))
+closeCsvFile eiHandle = do
+    return closeFileHandle
+  where
+        closeFileHandle = do
+            case eiHandle of
+                Left err     -> Left err
+                Right handle -> Right $ hClose handle
+
+parse :: ()
+      => Either T.Text Handle
+      -> IO (Either T.Text Handle)
+parse eiHandle = do
+    return parseFile
+  where
+        parseFile = do
+            case eiHandle of
+                Left err     -> Left err
+                Right handle -> Right handle
+
