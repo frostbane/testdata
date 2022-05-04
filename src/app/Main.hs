541c264fe1653aff789c1683970beb267d74fb5d
ǝuɐqʇsoɹɟ <frostbane@programmer.net>
2022-05-05 04:27:59 +1000

    spock config
    --HG--
    branch : develop

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -14,10 +14,27 @@ import qualified Network.HTTP.Types.Status as Http
 
 main :: IO ()
 main = do
-    cfg <- defaultSpockCfg () PCNoDatabase ()
+    sess <- defaultSessionCfg ()
+    let cfg = SpockCfg ()
+                   PCNoDatabase
+                   sess
+                   (Just 512000)
+                   errHandler
+                   errLogger
+                   True
+                   "x-akane"
+                   "x-akane"
+
     runSpock 3000 $ spock cfg app
   where
+    errLogger _ = return ()
+    errHandler status@(Http.Status code message)
+        | status == Http.notFound404  = text "Not Found 404"
+        | otherwise                   = text "error"
     app :: SpockM () () () ()
     app = do
         middleware $ staticPolicy (addBase "static")
+        get ("hello" <//> var) $
+            \name -> do
+                text ("Hello " <> name)
 
