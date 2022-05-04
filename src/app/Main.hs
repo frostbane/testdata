c0cd04a2d9c362f14753cf328cb14ebcf3f77dcf
frostbane <frostbane@programmer.net>
2022-05-05 03:32:49 +1000

    add static path

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -1,13 +1,23 @@
+{-# OPTIONS
+ -Wall
+ -Wno-unused-matches
+#-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
 import Web.Spock
 import Web.Spock.Config
+import Network.Wai.Middleware.Static
+import qualified Data.Text as T
+import qualified Network.HTTP.Types.Status as Http
 
 main :: IO ()
 main = do
     cfg <- defaultSpockCfg () PCNoDatabase ()
-    runSpock 8080 $ spock cfg app
+    runSpock 3000 $ spock cfg app
   where
     app :: SpockM () () () ()
-    app = return ()
+    app = do
+        middleware $ staticPolicy (addBase "static")
+
