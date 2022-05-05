62b2f52a11476d995b43d7897e7bcea83473916e
ǝuɐqʇsoɹɟ <frostbane@programmer.net>
2022-05-05 20:38:47 +1000

    refactoring
    --HG--
    branch : develop

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -3,49 +3,21 @@
  -Wno-unused-matches
 #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
-import Web.Spock
-import Web.Spock.Lucid (lucid)
-import Web.Spock.Config
-import Network.Wai.Middleware.Static
-import Lucid
-import Data.Text (Text)
-import qualified Data.Text as T
-import qualified Network.HTTP.Types.Status as Http
+import Server
+import Server.Actions as Actions
+import Web.Spock hiding (middleware)
+import Network.Wai
 
-createSpockCfg :: IO (SpockCfg () () ())
-createSpockCfg = do
-    defaultConf <- defaultSpockCfg () PCNoDatabase ()
-    let cfg = defaultConf { spc_maxRequestSize = Just 512000
-                          , spc_errorHandler   = errHandler
-                          , spc_logError       = errLogger
-                          , spc_csrfProtection = True
-                          , spc_csrfHeaderName = "x-requested-with"
-                          , spc_csrfPostName   = "x-csrf-token"
-                          }
-    return cfg
-  where
-    errLogger :: Text -> IO()
-    errLogger _ = return ()
-    errHandler :: Http.Status -> ActionCtxT () IO ()
-    errHandler status@(Http.Status code message)
-        | status == Http.notFound404  = h404
-        | otherwise                   = text "error"
-    h404 = lucid $ do
-        h1_ "404"
-        p_ "not found"
+
+middleware :: IO Middleware
+middleware = do
+    conf <- Server.createSpockCfg Actions.errorLogger Actions.errorHandler
+    spock conf Actions.basicHandler
 
 main :: IO ()
 main = do
-    cfg <- createSpockCfg
-    runSpock 3000 $ spock cfg app
-  where
-    app :: SpockM () () () ()
-    app = do
-        middleware $ staticPolicy (addBase "static")
-        get ("hello" <//> var) $
-            \name -> do
-                text ("Hello " <> name)
-
+    port <- Server.getHttpPort
+    runSpock port middleware
---
+++ src/lib/Fb/Environment.hs
@@ -0,0 +1,78 @@
+{-# OPTIONS -Wall -Wno-unused-matches #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Fb.Environment
+    ( loadEnv
+    , getEnvDefault
+    ) where
+
+import Prelude
+import System.Environment ( setEnv
+                          , lookupEnv
+                          )
+import System.IO ( openFile
+                 , hGetLine
+                 , hClose
+                 , hIsEOF
+                 , Handle
+                 , IOMode (ReadMode)
+                 )
+import Control.Exception ( try
+                         , SomeException
+                         , displayException
+                         )
+import qualified Data.Text as T
+import Data.Text (Text)
+
+
+splitPair :: String -> (String, String)
+splitPair line = (key, val)
+  where
+    pair = T.breakOn "=" $ T.pack line
+    key  = T.unpack $ T.strip $ fst pair
+    val  = T.unpack $ T.strip $ T.drop 1 $ snd pair
+
+parseEnv :: Handle -> IO Handle
+parseEnv handle = do
+    isEnd <- hIsEOF handle
+    if isEnd then return handle
+    else do
+        line <- hGetLine handle
+        let pair = splitPair line
+        if fst pair == "" then return handle
+        else do
+            uncurry setEnv pair
+            parseEnv handle
+
+{- | returning IO (Either Text (IO())) will execute the Right (IO())
+     only when needed (lazy)
+
+     so I executed the IO inside the function and returned
+     IO (Either Text ()) instead
+    -}
+loadEnv :: Text                 -- ^ environment file name
+        -> IO (Either Text ())
+loadEnv env = do
+    let filename = T.unpack env
+    fileOpenResult <- try (openFile filename ReadMode) :: IO (Either SomeException Handle)
+    loadResult <- loadFileEnv fileOpenResult
+    pure $ case fileOpenResult of
+        Left ex      -> Left $ T.pack $ displayException ex
+        Right handle -> Right loadResult
+  where
+    loadFileEnv result = case result of
+        Right handle -> hClose =<< parseEnv handle
+        _            -> return ()
+
+{-| get environment variable value or use default if not foun
+
+    environment key is case sensitived
+    -}
+getEnvDefault :: String    -- ^ environment key
+              -> String    -- ^ default value if key is not found
+              -> IO String
+getEnvDefault key def = coalesce =<< lookupEnv key
+  where
+    coalesce result = case result of
+        Just val -> return val
+        Nothing  -> return def
